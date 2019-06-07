70334f3a832343bcb6cd57d80ae66583494784fd
rafabrk <rafaelbrk.so@gmail.com>
2019-06-07 15:44:51 -0300

    [Finished]}

--- js/script.js
+++ js/script.js
@@ -4,9 +4,5 @@ document.onscroll = function(ev){
         document.body.style="background-position-y: " + (window.pageYOffset / 4) * -1 + "px";
     } 
 
-    var business__info = document.querySelector(".business__info");
-    business__info.style = "transform:translateY(" + (window.pageYOffset / 4) * -1 + "px)";
-
-
-    
-}
+     
+};
